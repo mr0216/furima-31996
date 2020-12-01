7ee0fa2bebbe90cd12e7861dd473e9a129fc0ad0
mr0216 <nnr0216@gmail.com>
2020-12-01 23:31:39 +0900

    ユーザー管理機能(Rubocop実施後)

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"tanaka"}
-    email                 {Faker::Internet.free_email}
-    password              {"a12345"}
-    password_confirmation {password}
-    last_name             {"全角"}
-    first_name            {"全角"}
-    last_name_kana        {"ゼンカクカナ"}
-    first_name_kana       {"ゼンカクカナ"}
-    birthday              {"1990-01-01"}
+    nickname              { 'tanaka' }
+    email                 { Faker::Internet.free_email }
+    password              { 'a12345' }
+    password_confirmation { password }
+    last_name             { '全角' }
+    first_name            { '全角' }
+    last_name_kana        { 'ゼンカクカナ' }
+    first_name_kana       { 'ゼンカクカナ' }
+    birthday              { '1990-01-01' }
   end
 end
